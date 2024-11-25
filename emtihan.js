47a1f8d42303f646436ef6fd4517d2c4388fe995
Berikboluly Myrza <berikbolmyrza@gmail.com>
2024-11-25 18:59:19 +0500

    first commit

---
+++ emtihan.js
@@ -0,0 +1,74 @@
+// function calculator() {
+//   let num1 = +prompt("Birinshi san engiz: ");
+//   let num2 = +prompt("Ekinshi san engiz: ");
+//   let tanba = prompt("tanba engiz: ");
+
+//   while (true) {
+//     if (num1 && num2 != Number()) {
+//         return "Қате енгізу!"
+//     } else if (tanba == "+") {
+//       alert(num1 + num2);
+//       break;
+//     } else if (tanba == "-") {
+//       alert(num1 - num2);
+//       break;
+//     } else if (tanba == "/") {
+//       alert(num1 / num2);
+//       break;
+//     } else if (tanba == "*") {
+//       alert(num1 * num2);
+//       break;
+//     }
+//   }
+// }
+
+// calculator();
+
+// function supermarket() {
+//   let price = +prompt("Zattyn bagasyn jazynyz: ");
+//   let amount = +prompt("Kansha zat algynyz keledi: ");
+//   let skidka = 0;
+
+//   skidka = price * amount
+
+//   if (5000 < 10000) {
+//     alert(skidka / 10);
+//   } else if (10000 < 20000) {
+//     alert(skidka / 5);
+//   } else if (20000 <= Infinity) {
+//     alert(skidka * 0.3);
+//   }
+
+// }
+
+// supermarket();
+
+// const secretNumber = () => {
+//   let secretNum = +prompt("1 men 100 sandar arasynda kypia sandy tap");
+
+//   while (true) {
+//     if (secretNum == 50) {
+//       alert("Jaraisyn, Durys!");
+//       break;
+//     } else if (secretNum >= 5) {
+//         alert("sal tomen")
+//     } else if (secretNum <= 5) {
+//         alert("sal jogary")
+//     }
+//   }
+// };
+
+// alert(secretNumber())
+
+
+
+// let a = 15
+// let b = 20
+
+// if (a = b) {
+//     b = 15
+// } else if (b = 20) {
+//     a = b
+// }
+
+// alert("a -> " + a + " " + " " + "b -> " + b)
